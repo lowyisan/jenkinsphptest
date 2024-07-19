0ca119f98ab4c42082ac08cb32aa0c03621cb65a
lowyisan <yisanlow@gmail.com>
2024-07-19 17:34:55 +0800

    :)

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -4,20 +4,19 @@ require 'GumballMachine.php';
 
 class GumballMachineTest extends TestCase {
 
-    public $gumballMachineInstance;
-    
-    public function setup():void {
-        
-        $this->gumballMachineInstance = new GumballMachine();
-    }
-    
-    public function testIfWheelWorks() {
-        
-        $this->gumballMachineInstance->setGumballs(100);
-        
-        $this->gumballMachineInstance->turnWheel();
-        
-        // Change the expected value to make the assert fail
-        $this->assertEquals(100, $this->gumballMachineInstance->getGumballs());
-    }
+	public $gumballMachineInstance;
+	
+	public function setup():void {
+		
+		$this->gumballMachineInstance = new GumballMachine();
+	}
+	
+	public function testIfWheelWorks() {
+		
+		$this->gumballMachineInstance->setGumballs(100);
+		
+		$this->gumballMachineInstance->turnWheel();
+		
+		$this->assertEquals(99, $this->gumballMachineInstance->getGumballs());
+	}
 }
